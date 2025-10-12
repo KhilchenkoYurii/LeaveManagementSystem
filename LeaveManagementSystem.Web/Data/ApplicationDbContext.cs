c4f719e9461b8b09bc31280cba7d24284ec62131
Khilchenko Yurii <38539304+KhilchenkoYurii@users.noreply.github.com>
2025-10-12 22:17:46 +0300

    Create Leave type model and CRUD functionals for it

--- LeaveManagementSystem.Web/Data/ApplicationDbContext.cs
+++ LeaveManagementSystem.Web/Data/ApplicationDbContext.cs
@@ -5,6 +5,8 @@ namespace LeaveManagementSystem.Web.Data
 {
     public class ApplicationDbContext : IdentityDbContext
     {
+        public DbSet<LeaveType> LeaveTypes { get; set; }
+
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
         {
